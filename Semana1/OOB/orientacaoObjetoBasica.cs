0c24d4ab2f866b18ab39cc8d30578723cef39aff
Leonardo Dognani <roberto.leonardo@venturus.org.br>
2022-02-25 14:15:55 -0300

    add oob

--- Semana1/OOB/orientacaoObjetoBasica.cs
+++ Semana1/OOB/orientacaoObjetoBasica.cs
@@ -1,12 +1,32 @@
 namespace semana1gh
 {
-    public class newclass
+    public class Program
     {
         static void Main(string[] args)
         {
             //Sobre classes, herança, métodos.
             // O que é uma classe abstrata e o que é uma interface?
             // quando devemos usar um ou outro?
+            
+/*          Classe
+            Classe é um modelo de um objeto do mundo real. É o bloco de construção básico da programação orientada a objetos e é reutilizável.
+            Uma classe C# define propriedades, campos, eventos, métodos, etc. Um objeto é uma instância de uma classe.
+            Modificadores de acesso definem a acessibilidade de uma classe: public, private, protected ou internal.
+            Um Namespace pode incluir uma ou mais classes.
+*/
+
+/*          Herança
+            Herança é um recurso das linguagens de programação orientadas a objeto que permite a definição de uma classe base que, por sua 
+            vez, fornece uma funcionalidade específica (dados e comportamento), e a definição de classes derivadas que herdam ou substituem 
+            essa funcionalidade.
+*/
+
+/*          Métodos
+            Um método é um bloco de código que contém uma série de instruções. Um programa faz com que as instruções sejam executadas chamando o 
+            método e especificando os argumentos de método necessários. No C#, todas as instruções executadas são realizadas no contexto de um método. 
+            O método Main é o ponto de entrada para todos os aplicativos C# e é chamado pelo CLR (Common Language Runtime) quando o programa 
+            é iniciado.
+*/
 
         }
     }
