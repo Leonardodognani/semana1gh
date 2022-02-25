814861a7710391ac11db41f62e12a391f39ce210
Leonardo Dognani <roberto.leonardo@venturus.org.br>
2022-02-25 11:39:54 -0300

    preparing oob

--- Semana1/OOB/orientacaoObjetoBasica.cs
+++ Semana1/OOB/orientacaoObjetoBasica.cs
@@ -2,6 +2,12 @@ namespace semana1gh
 {
     public class newclass
     {
-        
+        static void Main(string[] args)
+        {
+            //Sobre classes, herança, métodos.
+            // O que é uma classe abstrata e o que é uma interface?
+            // quando devemos usar um ou outro?
+
+        }
     }
 }
