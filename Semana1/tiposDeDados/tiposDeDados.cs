a693c8f4eaf8220d35548a823bd5ecf3d3554687
Leonardo Dognani <roberto.leonardo@venturus.org.br>
2022-02-25 11:12:12 -0300

    add data types

--- Semana1/tiposDeDados/tiposDeDados.cs
+++ Semana1/tiposDeDados/tiposDeDados.cs
@@ -1,1 +1,22 @@
-﻿
+﻿namespace semana1gh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //tipos de dados
+            int // Inteiro de 32-bit com sinal / -2.147.483.648 a 2.147.483.647
+            float //numeros de ponto flutuantes (32bits) / 7 dígitos significativos
+            double // números de ponto flutuantes (64 bits) / 15-16 dígitos significativos
+            decimal //valores monetários (128bits) / com sinal com 28-29 dígitos significativos
+            string // sequência de caracteres (16bits por caractere)
+            char // somente 1 caractere (16 bits)
+            bool // booleano (verdadeiro ou falso)
+
+            byte // -127 a 128 / Inteiro de 8-bit com sinal
+            long // Inteiro de 64-bit com sinal / –9,223,372,036,854,775,808 a 9,223,372,036,854,775,807
+            short // Inteiro de 16-bit com sinal / -32,768 a 32,767
+
+        }
+    }
+}
